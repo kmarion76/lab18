4a3742c30ba64c2051d71018b11ed1d612863be9
kmarion76 <47949532+kmarion76@users.noreply.github.com>
2021-03-08 17:16:30 -0500

    Lab 18

---
+++ Lab18/List.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab18
+{
+    public class List
+    {
+        private int _count = 0;
+        private readonly string[] _array;
+
+        public List(int maxLength)
+        {
+            _array = new string[maxLength];
+        }
+
+        public int Count()
+        {
+            return _count;
+        }
+
+        public bool IsEmpty()
+        {
+            return _count == 0;
+        }
+
+        public bool IsFull()
+        {
+            return _count == _array.Length;
+        }
+
+        public bool Insert(string value)
+        {
+            if (IsFull())
+            {
+                return false;
+            }
+
+            _array[_count] = value;
+            _count++;
+            return true;
+        }
+
+        //Lab 18 Number 1
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < _count; i++)
+            {
+                _array[i] = _array[i + 1];
+            }
+            foreach (var item in _array)
+            {
+                Console.WriteLine(item);
+            }
+            
+            _count--;
+        }
+
+        //Lab 18 Number 2
+        public void PrintReverse(string[] newArray)
+        {
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                Array.Reverse(newArray);
+            }
+            foreach (var item in newArray)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+       //Lab 18 Number 3
+        public bool InsertAt(int index, Object o)
+        {
+
+            o = "testing";
+            for (int i = index; i < _count; i++)
+            {
+                Console.WriteLine(i);
+            }
+            if (index > _count)
+            {
+                return false;
+            }
+            else
+            {
+                _array[_count] = (string)o;
+                _count++;
+                foreach (var item in _array)
+                {
+                    Console.WriteLine(item);
+                }
+                return true;
+            }
+            ;
+        }
+    }
+
+
+}
+
---
+++ Lab18/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] games = { "cod", "wow", "mario" };
+            List test = new List(10);
+            test.Insert("Hi");
+            test.Insert("my");
+            test.Insert("name");
+            test.Insert("is");
+            test.Insert("kevin");
+
+            //Test for number 1
+            test.RemoveAt(1);
+            //Test for number 2
+            test.PrintReverse(games);
+            //Test for number 3
+            test.InsertAt(1, "spiderman");
+            
+            
+        }
+    }
+}
